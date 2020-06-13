bcfda86ec6b1945b15ccf5107eb305c0027fb716
shengsheng555 <jinglun.cn@gmail.com>
2020-06-13 15:28:55 -0400

    init

---
+++ @studydefi/money-legos/idle/contracts.d.ts
@@ -0,0 +1,25 @@
+declare const contracts: {
+    maxYield: {
+        dai: {
+            address: string;
+        };
+        usdc: {
+            address: string;
+        };
+        usdt: {
+            address: string;
+        };
+    };
+    riskAdjusted: {
+        dai: {
+            address: string;
+        };
+        usdc: {
+            address: string;
+        };
+        usdt: {
+            address: string;
+        };
+    };
+};
+export default contracts;
